2cb181647706a1b86e11371ea54fc44ad893d746
mingzailao <mingzailao@163.com>
2015-12-05 17:09:37 +0800

    some err fixed

--- BSpline.py
+++ BSpline.py
@@ -96,7 +96,7 @@ ax.plot_wireframe(x1,y1,z1,color='b')
 ax.plot_wireframe(x2,y2,z2,color='g')
 plt.show()
 
-Points3=np.array([[ 0. ,  0. ,  0. ],[-1. ,  2. , -2. ],[ 2. ,  4. , -2.5],[ 0. ,  0. ,  0. ],[ 2. , -4. , -4. ],[ 5. , -6. ,  8. ]])
+
 
 
 
